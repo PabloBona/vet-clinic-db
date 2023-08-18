d4fa37a013fb68dbf7b500213869395cd0074c61
PabloBona <pablobonasera@gmail.com>
2023-08-17 21:37:57 -0300

    Created a 'vets', "specialitations" and "visits" table Add queries Insert data in data.sql

--- schema.sql
+++ schema.sql
@@ -37,21 +37,25 @@ ADD CONSTRAINT fk_animals_owners
 FOREIGN KEY (owner_id) REFERENCES owners(id);
 
 -- 4
+-- Create a new table called "vets"
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    age INT NOT NULL,
+    date_of_graduation DATE NOT NULL
+);
+
 --create a join table called "specializations"
 CREATE TABLE specializations (
-    vet_id INT,
-    species_id INT,
-    PRIMARY KEY (vet_id, species_id),
-    FOREIGN KEY (vet_id) REFERENCES vets (id),
-    FOREIGN KEY (species_id) REFERENCES species (id)
+    vet_id INT REFERENCES vets(id),
+    species_id INT REFERENCES species(id),
+    PRIMARY KEY (vet_id, species_id)
 );
 
 --create a join table called "visits
 CREATE TABLE visits (
-    animal_id INT,
-    vet_id INT,
+    animal_id INT REFERENCES animals(id),
+    vet_id INT REFERENCES vets(id),
     visit_date DATE,
-    PRIMARY KEY (animal_id, vet_id, visit_date),
-    FOREIGN KEY (animal_id) REFERENCES animals (id),
-    FOREIGN KEY (vet_id) REFERENCES vets (id)
+    PRIMARY KEY (animal_id, vet_id)
 );
